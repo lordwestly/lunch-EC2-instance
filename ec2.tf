411e58983d7385675f2daf1c9a11039044515d36
lordwestly <dandaveintegrateds@gmail.com>
2025-08-29 07:58:19 +0100

    update

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform-user"
+  region  = "eu-west-1"
+  profile = "default"
 }
 
 
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "west"
   user_data              = file("install_techmax.sh")
 
   tags = {
